af1bd423122ac6a451dfd36bc566dd4e06b6697f
Mateusz Targoński <mat.targonski@gmail.com>
2024-10-17 13:15:28 +0200

    Initial commit

---
+++ src/main/java/pl/umcs/medlai/MedlAiApplication.java
@@ -0,0 +1,13 @@
+package pl.umcs.medlai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedlAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedlAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/umcs/medlai/MedlAiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umcs.medlai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedlAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
