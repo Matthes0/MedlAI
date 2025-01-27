ae9ec72ecbed03e301d7cb9632af6ae64bedd2e2
Mateusz Targoński <matthes123321@gmail.com>
2025-01-27 20:14:29 +0100

    date fixes

--- springboot-backend/src/main/java/pl/umcs/medlai/dto/AppointmentBookedDTO.java
+++ springboot-backend/src/main/java/pl/umcs/medlai/dto/AppointmentBookedDTO.java
@@ -1,6 +1,7 @@
 package pl.umcs.medlai.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import pl.umcs.medlai.model.Status;
@@ -11,6 +12,8 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class AppointmentBookedDTO {
     private Integer doctor_id;
+    @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}",
+            message = "Date must follow the format yyyy-MM-dd'T'HH:mm:ss")
     @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime start_date;
     private String patient_first_name;
